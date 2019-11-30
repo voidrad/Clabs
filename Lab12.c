6c34850bc1d90a4436758e5b7cd40a0b6404124a
voidrad <semen.bugreev@mail.ru>
2019-11-30 16:38:10 +0300

    Initial commit

---
+++ Lab12.c
@@ -0,0 +1,28 @@
+//
+// Created by Semka on 30-Nov-19.
+//
+#include<stdio.h>
+
+int main{
+        long a,izn = 0;
+        int ch = 0;
+        int end = 0;
+        scanf("%d\n",a);
+        izn = a;
+
+        while(a>0){
+            ch = a%10;
+            a= a/10;
+            if()
+
+
+
+
+
+
+
+        }
+        printf("%d\n", );
+};
+
+More
---
+++ main.c
@@ -0,0 +1,33 @@
+//
+// Created by Semka on 29-Nov-19.
+//
+#include<stdio.h>
+#define zero '0'
+#define one '1'
+
+int main()
+{
+    int bool = -1; int count=0;
+
+    char c;
+
+
+    while( (c =getchar()) != EOF){
+
+        if(c>=zero && c<=one){
+            if (bool ==-1){
+                bool++;
+            }
+            bool++;
+            continue;
+        }else
+        {if (bool ==-1){
+                continue;
+            }
+            if(bool%4 ==0){
+                count++;
+            }
+            bool=-1;
+        }}
+    printf("%d\n", count);
+}
