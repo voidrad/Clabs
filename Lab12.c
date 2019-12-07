c83e709de326ad927e221d655de25492549b7696
voidrad <voidrad@mail.ru>
2019-12-07 15:03:22 +0300

    11+12Ready

--- Lab12.c
+++ Lab12.c
@@ -3,29 +3,50 @@
 //
 #include<stdio.h>
 
-int main{
-        long a,izn = 0;
+int main(){
+
+        long a = 0;
+        long izn = 0;
         int ch = 0;
-        int end = 0;
-        scanf("%d\n",a);
+        int kolch = 0;
+        int kolnc = 0;
+        int nc = 0;
+        long end = 0;
+        scanf("%d", &a);
 
         izn = a;
-
-
 
         while(a>0){
             ch = a%10;
             a= a/10;
-            if()
+            if(ch%2 ==0){
+                int i;
+                long step =10;
+                for (i = kolch;i>0;i--){
+                    step = step*10;
+                }
+                 nc = end%(step/10);
+                end = end/(step/10);
+                end = end*10+ch;
+                end = end*(step/10)+nc;
+                nc = 0;
+                 kolch++;
 
-
-
-
-
-
-
+            }else{
+                long step = 1;
+                int i;
+                for (i = (kolnc+kolch);i>0;i--){
+                    step = step*10;
+                }
+                nc =end%step;
+                end = end/step;
+                end = end*10+ch;
+                end = end*step+nc;
+                kolnc++;
+                nc = 0;
+            }
         }
-        printf("%d\n", );
+        printf("%d\n",end );
 };
 
 
